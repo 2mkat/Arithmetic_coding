3fea391baa25d42cff7d72c4ea30f1dfc1546339
Катя <58567818+2mkat@users.noreply.github.com>
2021-05-21 13:22:06 +0700

    Update decoder.cpp

--- decoder.cpp
+++ decoder.cpp
@@ -6,67 +6,81 @@
 
 using namespace std;
 
-struct Range{
+int h = 65535;
+int first_qtr = (h + 1) / 4, half = first_qtr * 2, third_qtr = first_qtr * 3;
+
+struct Table{
     char s;
-    int numb;
-    int lb;
-    int rb;
+    int n_s;
+    int a;
+    int b;
 };
 
-struct Sort{//сравнивает 2, если Sort=1 то левый меньше правого и если 0, то наоборот
-	bool operator() (Range l, Range r){
-		return l.numb > r.numb;
-	}
-};
-
+bool comp(Table l, Table r){
+    return l.n_s > r.n_s;
+}
 
 int main(){
-    int count = 0;
-    ifstream f("output.txt", ios::out | ios::binary);//открываем файл для чтения, который нужно декодировать
-	ofstream g("res.txt", ios::out | ios::binary);//открываем файл для записи декодированного текста
+    int n_symbols = 0;
+
+    // open files
+    std::ifstream file("output.txt", std::ios::out | std::ios::binary);
+    if (!file){
+        std::cerr << "Uh oh, output.txt could not be opened for reading!" << '\n';
+    }
+    std::ofstream res_file("res.txt", std::ios::out | std::ios::binary);
+    if(!res_file){
+        std::cerr << "Uh oh, res.txt could not be opened for writing!" << '\n';
+    }
+
+    // read symbols for freq table
     int x1, x2;
 	char s;
 	map<char, int> m;
-    map <char, int> ::iterator ii;														//мапа для таблицы кодирования
-	// считывание из файла code.txt и декодирование
-	f.read((char*)&x1, sizeof(x1));												//считываем первое число для расшифровки таблциы																//отнимаем значение int
+    map <char, int> ::iterator iterator;
+
+	file.read((char*)&x1, sizeof(x1));
     while (x1 > 0){
-		f.read((char*)&s, sizeof(s));											//считываем символ
-		f.read((char*)&x2, sizeof(x2));											//считываем его значение
-		x1 -= 40;																//отнимаем 8 бит символа и 32 его значения
-		m[s] = x2;                                                            //строим таблицу
+		file.read((char*)&s, sizeof(s));
+		file.read((char*)&x2, sizeof(x2));
+		--x1;
+		m[s] = x2;
     }
-    list<Range> L;
-    for(ii=m.begin(); ii!=m.end(); ii++){
-        Range p;
-        p.s=ii->first;
-        p.numb=ii->second;
-        L.push_back(p);
+
+    // building table
+    list<Table> table;
+    for(iterator = m.begin(); iterator != m.end(); iterator++){
+        Table t;
+        t.s = iterator->first;
+        t.n_s = iterator->second;
+        table.push_back(t);
     }
-    L.sort(Sort());
-    L.begin()->rb = L.begin()->numb;
-    L.begin()->lb = 0;
-    list<Range>::iterator it=L.begin(), i2;
-    i2 = it;
+    table.sort(comp);
+    table.begin()->b = table.begin()->n_s;
+    table.begin()->a = 0;
+    list<Table>::iterator it = table.begin(), it2;
+    it2 = it;
     it++;
-    for(; it!=L.end(); it++){
-        it->lb = i2->rb;
-        it->rb = it->lb + it->numb;
-        i2++;
+    for(; it != table.end(); it++){
+        it->a = it2->b;
+        it->b = it->a + it->n_s;
+        it2++;
     }
-    count = 0;
-    int l = 0, h = 65535, denom = L.back().rb;
-    int first_qtr = (h + 1) / 4, half = first_qtr * 2, third_qtr = first_qtr * 3;
-    int value = (f.get() << 8) | f.get();
-    char c = f.get();
-    while(!f.eof()){
+
+    // decompressor
+    n_symbols = 0;
+    int l = 0, denom = table.back().b;
+
+    int value = (file.get() << 8) | file.get();
+    char c = file.get();
+    while(!file.eof()){
         int freq = ((value - l + 1) * denom - 1) / (h - l + 1);
-        for(it = L.begin(); it->rb <= freq; it++);
+        for(it = table.begin(); it->b <= freq; it++);   // поиск слова
         int l2 = l;
-        l = l + (it->lb) * (h - l + 1) / denom;
-        h = l2 + (it->rb) * (h - l2 + 1) / denom - 1;
-        for(;;){
-            if(h < half);
+        l = l + (it->a) * (h - l + 1) / denom;
+        h = l2 + (it->b) * (h - l2 + 1) / denom - 1;
+        for(;;){    // обработка вариантов
+            if(h < half);   // если переполнение, ничего не делаем
             else if(l >= half){
                 l -= half;
                 h -= half;
@@ -81,16 +95,17 @@ int main(){
             }
             l += l;
             h += h + 1;
-            value += value + (((short)c >> (7 - count)) & 1);
-            count++;
-            if(count == 8){
-                c = f.get();
-                count = 0;
+            value += value + (((short)c >> (7 - n_symbols)) & 1);
+            n_symbols++;
+            if(n_symbols == 8){
+                c = file.get();
+                n_symbols = 0;
             }
         }
-        g << it->s;
+        res_file << it->s;
     }
-    f.close();
-    g.close();
+
+    file.close();
+    res_file.close();
     return 0;
 }
