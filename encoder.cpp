59e01707aa863eb44a4ef8617ded2bad56c89582
Катя <58567818+2mkat@users.noreply.github.com>
2021-05-25 12:31:48 +0700

    Update encoder.cpp

--- encoder.cpp
+++ encoder.cpp
@@ -2,58 +2,69 @@
 #include <vector>
 #include <map>
 #include <list>
+#include <algorithm>
 
 #include "header.cpp"
+
+void write_bit_to_file(std::ostream& g, Variable& v, int& count) {
+    count = 0;
+    g << v.temp;
+    v.temp = 0;
+}
+
+void add_bits(std::ostream& g, Variable& v, int& count) {
+    v.temp = v.temp | (1 << (7 - count));
+    count++;
+    if(count == 8){
+        write_bit_to_file(g, v, count);
+    }
+}
+
+void check_count(std::ostream& g, Variable& v, int& count) {
+    count++;
+    if(count == 8){
+        write_bit_to_file(g, v, count);
+    }
+}
 
 //procedure for transferring the found bits to left_edge file
 void bits_plus_follow(int bit, int &count, std::ofstream &g, Variable& v) {
     if(bit == 0){
-        count++;
-        if(count == 8){
-            count = 0;
-            g << v.temp;
-            v.temp = 0;
-        }
-        for(; v.bits_to_follow > 0; v.bits_to_follow--) {
-            v.temp = v.temp | (1 << (7 - count));
-            count++;
-            if(count == 8){
-                count = 0;
-                g << v.temp;
-                v.temp = 0;
-            }
+        check_count(g, v, count);
+        while(v.bits_to_follow > 0){
+            add_bits(g, v, count);
+            v.bits_to_follow--;
         }
     }
     else if(bit == 1) {
-        v.temp = v.temp | (1 << (7 - count));
-        count++;
-        if (count == 8) {
-            count = 0;
-            g << v.temp;
-            v.temp = 0;
-        }
-        for (; v.bits_to_follow > 0; v.bits_to_follow--) {
-            count++;
-            if (count == 8) {
-                count = 0;
-                g << v.temp;
-                v.temp = 0;
-            }
+        add_bits(g, v, count);
+        while(v.bits_to_follow > 0) {
+            check_count(g, v, count);
+            v.bits_to_follow--;
         }
     }
 }
 void compress_data(std::ifstream& file,  std::ofstream& res_file, std::list<Table>& table) {
     Variable v;
-    int idx = 0, denom = table.back().right_edge, low = 0;
+    int denom = table.back().right_edge, low = 0;  // low - нижняя граница отрезка, denom - делитель
     char temp = 0;
     int n_symbols = 0;
-    auto it = table.begin();
+    auto it = table.begin();    // итератор для для списка
 
-    file.clear();
+    file.clear();   // сбрасываем указатель в начало файла
     file.seekg(0);
 
     while(!file.eof()){
-        char c = file.get(); idx++;
+        char c = file.get();
+
+        /*it = std::find_if(table.begin(), table.end(), [c](const Table& c1){return c1.symbol == c;});
+        if (it != table.end()) {
+            continue;
+        }
+        else {
+            std::cout << "Element not found in table\n";
+        }*/
+
         for(it = table.begin(); it != table.end(); it++){
             if(c == it->symbol){
                 break;
@@ -63,10 +74,11 @@ void compress_data(std::ifstream& file,  std::ofstream& res_file, std::list<Tabl
             std::cout << "Error!" << '\n';
             break;
         }
+
         int l2 = low;
         low = low + it->left_edge * (v.high - low + 1) / denom;
         v.high = l2 + it->right_edge * (v.high - l2 + 1) / denom - 1;
-        for(;;){    // обрабатываем варианты
+        while(true) {    // обрабатываем варианты
             if(v.high < v.half){   //переполнение
                 bits_plus_follow(0, n_symbols, res_file, v);
             }
@@ -111,11 +123,8 @@ int main(){
     //create table with intervals
     std::list<Table> table = build_table(frequencies);
 
-    //get information about symbols
-    int count = count_symbols(frequencies);
-
     // write header
-    write_to_file(res_file,count, frequencies);
+    write_to_file(res_file, frequencies.size(), frequencies);
 
     // start compress file
     compress_data(file, res_file, table);
